1479046a1248375f6369ae504364817166c030d8
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/SettingManager.php
+++ src/Managers/SettingManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Setting newEntity()
+ * @method \Amethyst\Schemas\SettingSchema getSchema()
+ * @method \Amethyst\Repositories\SettingRepository getRepository()
+ * @method \Amethyst\Serializers\SettingSerializer getSerializer()
+ * @method \Amethyst\Validators\SettingValidator getValidator()
+ * @method \Amethyst\Authorizers\SettingAuthorizer getAuthorizer()
+ */
 class SettingManager extends Manager
 {
     use ConfigurableManager;
